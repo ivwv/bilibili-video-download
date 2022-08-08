7292b1efecbea82894242746607058f28786198f
sy <sy18959889840@gmail.com>
2022-08-08 17:21:19 +0800

    :bulb:

--- v2/app.js
+++ v2/app.js
@@ -2,8 +2,3 @@ const BUtils = require("./BUtils.js");
 const config = require("./config.js");
 const { log } = console;
 const bUtils = new BUtils(config);
-// (async () => {
-//   bUtils.downloadVideo();
-//   // console.log(data);
-// })();
-// console.log(process.argv);
